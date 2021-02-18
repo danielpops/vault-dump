485114adc22d30f9aba01e834c5bcbc92ec9a0f7
Daniel Popescu <dpopes@yelp.com>
2021-02-17 18:28:00 -0800

    Initial commit of scaffolding for the project

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["requests"]
+
+setup(
+    name="vault-dump",
+    version="0.0.1",
+    author="Daniel Popescu",
+    author_email="danielpops@gmail.com",
+    description="A package to dump the configuration settings for a running hashicorp vault instance",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danielpops/vault-dump",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+    ],
+)
